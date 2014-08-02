c5bdefe6d1b539c1692de02550831ab8d0fc750c
Aravind Venkateswaran <aravindh@codeaurora.org>
2014-08-01 17:20:51 -0700

    msm: mdss: use helper functions to check for panel power state
    Whenever the display always-on feature is enabled, it is possible
    that the display pipeline can be in an intermediate state between being
    fully interactive to fully turned off. In the current implementation, a
    number of state transition decisions are made based on whether the
    display pipeline is on or not. Instead of explicitly checking for the
    panel power state, define generic helper functions which classify the
    panel power state into three different stages - interactive, low-power
    and off. Replace any existing check for the panel power state with these
    newly defined helper functions.
    
    Change-Id: I4d6dd883ea6aca8abd57fa048a79e0c88da09a95
    Signed-off-by: Aravind Venkateswaran <aravindh@codeaurora.org>
    
    Conflicts:
    	drivers/video/msm/mdss/mdp3_ctrl.c
    	drivers/video/msm/mdss/mdss_fb.c

--- drivers/video/msm/mdss/mdss_mdp_overlay.c
+++ drivers/video/msm/mdss/mdss_mdp_overlay.c
@@ -946,7 +946,7 @@ static int mdss_mdp_overlay_set(struct msm_fb_data_type *mfd,
 	if (ret)
 		return ret;
 
-	if (!mdss_fb_is_panel_power_on(mfd)) {
+	if (mdss_fb_is_power_off(mfd)) {
 		mutex_unlock(&mdp5_data->ov_lock);
 		return -EPERM;
 	}
@@ -1618,7 +1618,7 @@ static int mdss_mdp_overlay_unset(struct msm_fb_data_type *mfd, int ndx)
 		goto done;
 	}
 
-	if (!mdss_fb_is_panel_power_on(mfd)) {
+	if (mdss_fb_is_power_off(mfd)) {
 		ret = -EPERM;
 		goto done;
 	}
@@ -1765,7 +1765,7 @@ static int mdss_mdp_overlay_play(struct msm_fb_data_type *mfd,
 	if (ret)
 		return ret;
 
-	if (!mdss_fb_is_panel_power_on(mfd)) {
+	if (mdss_fb_is_power_off(mfd)) {
 		ret = -EPERM;
 		goto done;
 	}
@@ -1907,7 +1907,7 @@ static void mdss_mdp_overlay_pan_display(struct msm_fb_data_type *mfd)
 	if (mutex_lock_interruptible(&mdp5_data->ov_lock))
 		return;
 
-	if ((!mdss_fb_is_panel_power_on(mfd)) &&
+	if ((mdss_fb_is_power_off(mfd)) &&
 		!((mfd->dcm_state == DCM_ENTER) &&
 		(mfd->panel.type == MIPI_CMD_PANEL))) {
 		mutex_unlock(&mdp5_data->ov_lock);
@@ -2625,7 +2625,7 @@ static int mdss_mdp_histo_ioctl(struct msm_fb_data_type *mfd, u32 cmd,
 
 	switch (cmd) {
 	case MSMFB_HISTOGRAM_START:
-		if (!mdss_fb_is_panel_power_on(mfd))
+		if (mdss_fb_is_power_off(mfd))
 			return -EPERM;
 
 		if (mdata->needs_hist_vote && (mdata->reg_bus_hdl)) {
@@ -2664,7 +2664,7 @@ static int mdss_mdp_histo_ioctl(struct msm_fb_data_type *mfd, u32 cmd,
 		break;
 
 	case MSMFB_HISTOGRAM:
-		if (!mdss_fb_is_panel_power_on(mfd))
+		if (mdss_fb_is_power_off(mfd))
 			return -EPERM;
 
 		ret = copy_from_user(&hist, argp, sizeof(hist));
@@ -2698,7 +2698,7 @@ static int mdss_fb_set_metadata(struct msm_fb_data_type *mfd,
 			ret = -EINVAL;
 		break;
 	case metadata_op_crc:
-		if (!mdss_fb_is_panel_power_on(mfd))
+		if (mdss_fb_is_power_off(mfd))
 			return -EPERM;
 		ret = mdss_misr_set(mdata, &metadata->data.misr_request, ctl);
 		break;
@@ -2764,7 +2764,7 @@ static int mdss_fb_get_metadata(struct msm_fb_data_type *mfd,
 		}
 		break;
 	case metadata_op_crc:
-		if (!mdss_fb_is_panel_power_on(mfd))
+		if (mdss_fb_is_power_off(mfd))
 			return -EPERM;
 		ret = mdss_misr_get(mdata, &metadata->data.misr_request, ctl);
 		break;
@@ -2896,7 +2896,7 @@ static int __handle_overlay_prepare(struct msm_fb_data_type *mfd,
 	if (ret)
 		return ret;
 
-	if (!mdss_fb_is_panel_power_on(mfd)) {
+	if (mdss_fb_is_power_off(mfd)) {
 		mutex_unlock(&mdp5_data->ov_lock);
 		return -EPERM;
 	}
@@ -3297,7 +3297,7 @@ static int mdss_mdp_overlay_on(struct msm_fb_data_type *mfd)
 		ctl = mdp5_data->ctl;
 	}
 
-	if (mdss_fb_is_panel_power_on(mfd)) {
+	if (mdss_fb_is_power_on(mfd)) {
 		pr_debug("panel was never turned off\n");
 		rc = mdss_mdp_ctl_start(mdp5_data->ctl, false);
 		goto panel_on;
@@ -3360,7 +3360,7 @@ static int mdss_mdp_overlay_off(struct msm_fb_data_type *mfd)
 	 */
 	pm_runtime_get_sync(&mfd->pdev->dev);
 
-	if (mfd->panel_power_state != MDSS_PANEL_POWER_OFF) {
+	if (mdss_fb_is_power_on_lp(mfd)) {
 		pr_debug("panel not turned off. keeping overlay on\n");
 		goto ctl_stop;
 	}
@@ -3409,7 +3409,7 @@ ctl_stop:
 #endif
 	rc = mdss_mdp_ctl_stop(mdp5_data->ctl, mfd->panel_power_state);
 	if (rc == 0) {
-		if (mfd->panel_power_state == MDSS_PANEL_POWER_OFF) {
+		if (mdss_fb_is_power_off(mfd)) {
 			mutex_lock(&mdp5_data->list_lock);
 			__mdss_mdp_overlay_free_list_purge(mfd);
 			mutex_unlock(&mdp5_data->list_lock);
