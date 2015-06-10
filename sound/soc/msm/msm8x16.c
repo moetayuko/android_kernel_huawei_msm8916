875f9d20e9fd87d328b35e02186422054aa63e19
dianlujitao <dianlujitao@gmail.com>
2015-06-10 23:16:40 +0800

    ASoC: msm8x16: Use button gpio from CAF

--- sound/soc/msm/msm8x16.c
+++ sound/soc/msm/msm8x16.c
@@ -1167,7 +1167,6 @@ static void *def_msm8x16_wcd_mbhc_cal(void)
 	 * all btn_low corresponds to threshold for current source
 	 * all bt_high corresponds to threshold for Micbias
 	 */
-#ifndef CONFIG_HUAWEI_KERNEL
 	btn_low[0] = 25;
 	btn_high[0] = 25;
 	btn_low[1] = 50;
@@ -1178,19 +1177,6 @@ static void *def_msm8x16_wcd_mbhc_cal(void)
 	btn_high[3] = 112;
 	btn_low[4] = 137;
 	btn_high[4] = 137;
-#else
-	/* reset button range for huawei three button headset */
-	btn_low[0] = 0;
-	btn_high[0] = 87;
-	btn_low[1] = 87;
-	btn_high[1] = 212;
-	btn_low[2] = 212;
-	btn_high[2] = 330;
-	btn_low[3] = 330;
-	btn_high[3] = 330;
-	btn_low[4] = 330;
-	btn_high[4] = 330;
-#endif
 
 	return msm8x16_wcd_cal;
 }
